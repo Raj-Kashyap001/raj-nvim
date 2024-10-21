d79d7e12b8bbe662850f1f0699eb9f43d8f5a36f
Raj Kashyap <rajkashyapatservice@outlook.com>
2024-10-21 09:30:15 +0530

    Version 2
    Integrated Colorscheme to plugin/init.lua

--- lua/raj/plugins/init.lua
+++ lua/raj/plugins/init.lua
@@ -1,4 +1,54 @@
+-- lua/raj/plugins/init.lua
 return {
-  "nvim-lua/plenary.nvim", -- lua utility funcion for other plugins
-  "christoomey/vim-tmux-navigator", --tmux window managment keybind in vim
+    "nvim-lua/plenary.nvim", -- Lua utility
+    "christoomey/vim-tmux-navigator", -- Tmux navigator
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha", -- Choose between 'latte', 'frappe', 'macchiato', 'mocha'
+                transparent_background = true,
+                term_colors = true,
+                no_italic = false,
+                integrations = {
+                    treesitter = true,
+                    native_lsp = {
+                        enabled = true,
+                        virtual_text = {
+                            errors = { "italic" },
+                            hints = { "italic" },
+                            warnings = { "italic" },
+                            information = { "italic" },
+                        },
+                        underlines = {
+                            errors = { "underline" },
+                            hints = { "underline" },
+                            warnings = { "underline" },
+                            information = { "underline" },
+                        },
+                    },
+                    lsp_trouble = true,
+                    cmp = true,
+                    gitsigns = true,
+                    telescope = true,
+                    nvimtree = true,
+                    dashboard = true,
+                    bufferline = true,
+                    markdown = true,
+                    illuminate = true,
+                    hop = true,
+                    which_key = true,
+                    indent_blankline = {
+                        enabled = true,
+                        colored_indent_levels = false,
+                    },
+                },
+            })
+
+            -- Apply the theme
+            vim.cmd("colorscheme catppuccin-mocha")
+        end,
+    },
 }
